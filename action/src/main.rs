bb490a1366b13a7ebe06dc9365f3ff0e82fe2b4a
Adam Chalmers <adam.s.chalmers@gmail.com>
2022-09-24 13:42:50 -0500

    My first action is ready

--- action/src/main.rs
+++ action/src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let name = args.next().unwrap_or_else(|| {
+        eprintln!("Missing arg 1, the name");
+        exit(1);
+    });
+    println!("Hello {name}");
+    let time = std::time::Instant::now();
+    println!("::set-output name=time::{time:?}")
 }
+
